b686bc88dcae7fa8468b566eb1ec4f5fc1b83dc2
Latnipgit <gitlatnip91@gmail.com>
2023-12-20 18:33:02 +0530

    add reslecter labry and reducer update

--- src/store/debtors/debtors.selecter.js
+++ src/store/debtors/debtors.selecter.js
@@ -1,1 +1,22 @@
-export const selectDebtorsList = (state) => state.DebtorsReducer.debtors != undefined ? state.DebtorsReducer.debtors.response : [];
+import { createSelector } from 'reselect';
+
+const selectDebtorsReducer = (state) => state.DebtorsReducer;
+
+export const selectDebtorsList = createSelector(
+    [selectDebtorsReducer],
+    (DebtorsReducer) => DebtorsReducer.debtors.response
+)
+
+export const selectInvoiceList = createSelector(
+    [selectDebtorsReducer],
+    (DebtorsReducer) => DebtorsReducer.getInvoiceList.response
+)
+
+export const selectReportDefOpen = createSelector(
+    [selectDebtorsReducer],
+    (DebtorsReducer) => DebtorsReducer.isReportDefOpen
+)
+
+/* export const selectDebtorsList = (state) => state.DebtorsReducer.debtors != undefined ? state.DebtorsReducer.debtors.response : [];
+export const selectInvoiceList = (state) => state.DebtorsReducer.getInvoiceList != undefined ? state.DebtorsReducer.getInvoiceList.response : []
+export const selectReportDefOpen = (state) => state.DebtorsReducer.isReportDefOpen; */
