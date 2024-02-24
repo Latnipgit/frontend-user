c3ce51b9a073bfe4991c66a57d8b6ee5df23af73
harsit0150 <harshit0150@gmail.com>
2024-02-24 14:28:30 +0530

    New Ui of disputed transactions

--- src/pages/admin/ReportMeDefualter/markOtherReason.js
+++ src/pages/admin/ReportMeDefualter/markOtherReason.js
@@ -9,6 +9,8 @@ import {
   ModalFooter,
   ModalHeader,
   InputGroup,
+  FormGroup,
+  Form,
   Input,
   Label,
   Card,
@@ -17,6 +19,8 @@ import {
   Table,
   Row, Col
 } from "reactstrap"
+import { Link } from "react-router-dom";
+
 import { ToastContainer, toast } from 'react-toastify';
 import { useEffect } from "react";
 import 'react-datepicker/dist/react-datepicker.css';
@@ -24,11 +28,13 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { useSelector, useDispatch } from "react-redux"
 import { recoredPaymentReportDefault } from "../../../store/debtors/debtors.actions"
 
-
+import Dropzone from "react-dropzone";
 
 export const MarkOtherReasonModel = props => {
   const { isOpen, toggle, setIsOpenmark, selected, submitCheck } = props
   const dispatch = useDispatch()
+  const [radioOption, setRadioOption] = useState("")
+  const [selectedFiles, setselectedFiles] = useState([])
 
   const [salutations, setsalutations] = useState([
     { label: "Cash", value: "Cash" },
@@ -40,7 +46,9 @@ export const MarkOtherReasonModel = props => {
   useEffect(() => {
     // dispatch()
   }, [])
-
+  function handleToggleItem(radio) {
+    setRadioOption(radio)
+}
   const [attachment, setAttachment] = useState('')
   const [textBox, setTextBox] = useState("")
   const [error, setError] = useState("")
@@ -73,8 +81,31 @@ export const MarkOtherReasonModel = props => {
 
       })
   }
+  function handleAcceptedFiles(files) {
+    
+    console.log("filesfilesfiles",files)
+    files.map(file =>
+      Object.assign(file, {
+        preview: URL.createObjectURL(file),
+        formattedSize: formatBytes(file.size),
+      })
+    )
+
+    // setselectedFiles(selectedFiles,...files)
+  // setselectedFiles(...selectedFiles,files)
+  setselectedFiles(previous => [...previous, files])
+  }
 
 
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return "0 Bytes"
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i]
+  }
   const handleSubmit = () => {
     var size = Object.keys(attachment).length;
     if (size > 0) {
@@ -121,8 +152,13 @@ export const MarkOtherReasonModel = props => {
     }
   }
 
+  const deleteFn = (val) => { // val result index of component and item array
+    const assoc = [...selectedFiles];
+    assoc.splice(val, 1);
+    setselectedFiles(assoc);
+  }
 
-
+console.log("selectedFilesselectedFilesselectedFiles",selectedFiles)
 
 
   return (
@@ -141,11 +177,51 @@ export const MarkOtherReasonModel = props => {
         <ModalBody>
           <form>
             <Row>
+            <FormGroup tag="fieldset">
+                                        <FormGroup check className="mb-2">
+                                            <Input
+                                                name="radio1"
+                                                type="radio"
+                                                onChange={() => handleToggleItem("radio1")}
+                                            />
+                                            {' '}
+                                            <Label check>
+                                            Poor Quality of Product Recieve
+                                            </Label>
+                                        </FormGroup>
+                                        <FormGroup check className="mb-2">
+                                            <Input
+                                                name="radio1"
+                                                type="radio"
+                                                onChange={() => handleToggleItem("radio2")}
+                                            />
+                                            {' '}
+                                            <Label check>
+                                            Goods Recieved in Damage Conditions
+                                            </Label>
+                                        </FormGroup>
+                                        <FormGroup
+                                            check
+                                            className="mb-2"
+                                        >
+                                            <Input
+                                                name="radio1"
+                                                type="radio"
+                                                onChange={() => handleToggleItem("radio3")}
+                                            />
+                                            {' '}
+                                            <Label check>
+                                            Quantity is Less then Invoice Quantity
+                                            </Label>
+                                        </FormGroup>
+                                    </FormGroup>
+            </Row>
+            <Row>
               <Col className="selectionListss">
                 <Col md={3}>
                   <div className="mb-2"><b className="mt-2">Your reason</b></div>
                 </Col>
-                <Col md={12}>
+                <Col md={10}>
                   <div className="d-inline">
 
                     <Input
@@ -155,18 +231,22 @@ export const MarkOtherReasonModel = props => {
                       name="customerEmail"
                       value={textBox}
                       onChange={(e) => textBoxModule(e.target.value)}
-                      placeholder="Mation your reason"
+                      placeholder="Describe your Reason"
+                      style={{height:"100px"}}
                     />
                   </div>
                   {error != "" ? <div className="text-danger mt-2">{error}</div> : ""}
                 </Col>
               </Col>
-              <Col className="selectionListss mb-2 mt-2">
+             
+            </Row>
+            <Row>
+               <Col className="selectionListss mb-2 mt-2">
                 <Col md={8}>
                   <div className="mb-2"><b className="mt-2">Upload Supported Document File</b></div>
                 </Col>
                 <Col md={10} >
-                  <div className="d-inline">
+                  {/* <div className="d-inline">
                     <label
                       className="visually-hidden custom-content"
                       htmlFor="customerSelect"
@@ -190,11 +270,77 @@ export const MarkOtherReasonModel = props => {
                       Choose a file to upload (PDF, PNG, JPG, JPEG).
                     </div>
                     <p className="text-danger" style={{ fontSize: '11px' }}>Please upload all Documents in a single Pdf</p>
-                  </div>
+                  </div> */}
                 </Col>
                 <Col md={3}>
                 </Col>
               </Col>
+            </Row>
+            <Row>
+                    <Dropzone
+                      onDrop={acceptedFiles => {
+                        handleAcceptedFiles(acceptedFiles)
+                      }}
+                    >
+                      {({ getRootProps, getInputProps }) => (
+                        <div className="dropzone">
+                          <div
+                            className="dz-message needsclick"
+                            {...getRootProps()}
+                          >
+                            <input {...getInputProps()} />
+                            <div className="dz-message needsclick">
+                              <div className="mb-3">
+                                <i className="display-4 text-muted bx bxs-cloud-upload" />
+                              </div>
+                              <h4>Drop files here or click to upload.</h4>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </Dropzone>
+
+                 
+                    <div className="dropzone-previews mt-3" id="file-previews">
+                      {selectedFiles.map((f, i) => {
+                        return (
+                          <Card
+                            className="mt-1 mb-0 shadow-none border dz-processing dz-image-preview dz-success dz-complete"
+                            key={i + "-file"}
+                          >
+                            <div className="p-2">
+                              <Row className="align-items-center">
+                                <Col className="col-auto">
+                                  <img
+                                    data-dz-thumbnail=""
+                                    height="80"
+                                    className="avatar-sm rounded bg-light"
+                                    src={f[0].preview}
+                                  />
+                                </Col>
+                             
+                                <Col>
+                                  <Link
+                                    to="#"
+                                    className="text-muted font-weight-bold"
+                                  >
+                                    {f[0].name}
+                                  </Link>
+                                  <p className="mb-0">
+                                    <strong>{f[0].formattedSize}</strong>
+                                  </p>
+                                </Col>
+                                <Col className=" text-end">
+                                <button className="btn btn-info" type="button" onClick={() => deleteFn(i)}>
+                                  Delete
+                                </button>
+                                </Col>
+                              </Row>
+                            </div>
+                          </Card>
+                        )
+                      })}
+                    </div>
             </Row>
           </form>
         </ModalBody>
